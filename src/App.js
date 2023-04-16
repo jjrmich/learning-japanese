f86f4347fd5dc31345a3bca52ffece7801627ee2
Joshua Richter <richte81@cse.msu.edu>
2023-04-16 14:32:32 -0400

    initial commit, first two sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { words } from './words';
 
 function App() {
+  function renderJapanese(jpn) {
+    if (typeof jpn === 'string' || jpn instanceof String) {
+      return jpn;
+    }
+    else {
+      const result = [];
+      jpn.forEach(val => {
+        if (typeof val === 'string' || val instanceof String) {
+          result.push(val);
+        }
+        else {
+          // handle ruby characters
+            result.push(<ruby>{val[0]}<rt>{val[1]}</rt></ruby>);
+        }
+      })
+      return result;
+    }
+  }
+
+  function renderWords(section) {
+    return words[section].map(word => (
+      <div className='col-sm-4 col-md-2 gy-3'>
+        <div className='japanese-card card text-center'>
+          <div className='card-body'>
+            <h3 className='card-title'>
+              {/* <ruby>{word[0][0]}<rt>{word[0][1]}</rt></ruby> */}
+              {renderJapanese(word.japanese)}
+            </h3>
+            <p class='card-text'>{word.english}</p>
+          </div>
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container py-4'>
+        <div className='row mb-5'>
+          {renderWords('Hiragana 1')}
+        </div>
+        <div className='row'>
+          {renderWords('Hiragana 2')}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/words.js
@@ -0,0 +1,110 @@
+export const words = {
+  "Hiragana 1": [
+    {
+      japanese: [["一", "いち"]],
+      english: "one"
+    },
+    {
+      japanese: [["二", "に"]],
+      english: "two"
+    },
+    {
+      japanese: [["三", "さん"]],
+      english: "three"
+    },
+    {
+      japanese: [["四", "よん"]],
+      english: "four"
+    },
+    {
+      japanese: [["六", "ろく"]],
+      english: "six"
+    },
+    {
+      japanese: [["七", "なな"]],
+      english: "seven"
+    },
+    {
+      japanese: "おはよう",
+      english: "good morning"
+    },
+    {
+      japanese: "はい",
+      english: "yes"
+    },
+    {
+      japanese: [["白", "しろ"]],
+      english: "white"
+    },
+    {
+      japanese: [["赤", "あか"]],
+      english: "red"
+    },
+    {
+      japanese: [["青", "あお"]],
+      english: "blue"
+    }
+  ],
+  "Hiragana 2": [
+    {
+      japanese: [["読", "よ"],"む"],
+      english: "to read"
+    },
+    {
+      japanese: [["夜", "よる"]],
+      english: "night"
+    },
+    {
+      japanese: [["着", "き"],"る"],
+      english: "to wear"
+    },
+    {
+      japanese: [["野", "や"],["菜", "さい"]],
+      english: "vegetable"
+    },
+    {
+      japanese: ["お",["酒", "さけ"]],
+      english: "alcohol"
+    },
+    {
+      japanese: [["飲", "の"],"む"],
+      english: "to drink"
+    },
+    {
+      japanese: [["好", "す"],"き"],
+      english: "like"
+    },
+    {
+      japanese: [["寿", "す"],["司", "し"]],
+      english: "sushi"
+    },
+    {
+      japanese: [["照", "て"],"り",["焼", "や"],"き"],
+      english: "teriyaki"
+    },
+    {
+      japanese: [["甘", "あま"],"い"],
+      english: "sweet"
+    },
+    {
+      japanese: [["欲", "ほ"],"しい"],
+      english: "want"
+    },
+    {
+      japanese: [["汚", "きたな"],"い"],
+      english: "dirty"
+    },
+    {
+      japanese: [["私", "わたし"]],
+      english: "I/me"
+    },
+    {
+      japanese: "さようなら",
+      english: "goodbye"
+    },
+    {
+      japanese: "こんにちは",
+      english: "hello/good afternoon"
+    }
+  ]
+}
