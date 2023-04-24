8c4f74582e739d2f8736aac801c0092e31a23a3b
Joshua Richter <richte81@cse.msu.edu>
2023-04-23 22:06:04 -0400

    more stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { words } from './words';
 function App() {
   function renderJapanese(jpn) {
     if (typeof jpn === 'string' || jpn instanceof String) {
-      return jpn;
+      return <ruby className='japanese-translation'>{jpn}<rt>　</rt></ruby>;
     }
     else {
       const result = [];
@@ -23,14 +23,14 @@ function App() {
 
   function renderWords(section) {
     return words[section].map(word => (
-      <div className='col-sm-4 col-md-2 gy-3'>
+      <div className='col-6 col-md-4 col-lg-3 col-xl-3 py-2'>
         <div className='japanese-card card text-center'>
           <div className='card-body'>
             <h3 className='card-title'>
               {/* <ruby>{word[0][0]}<rt>{word[0][1]}</rt></ruby> */}
               {renderJapanese(word.japanese)}
             </h3>
-            <p class='card-text'>{word.english}</p>
+            <p class='card-text english-text'>{word.english}</p>
           </div>
         </div>
       </div>
@@ -40,11 +40,29 @@ function App() {
   return (
     <div className="App">
       <div className='container py-4'>
-        <div className='row mb-5'>
+        <h2 className='section-title'>Hiragana 1</h2>
+        <div className='row japan-row py-2'>
           {renderWords('Hiragana 1')}
         </div>
-        <div className='row'>
+        <hr />
+        <h2 className='section-title blue'>Hiragana 2</h2>
+        <div className='row japan-row py-2'>
           {renderWords('Hiragana 2')}
+        </div>
+        <hr/>
+        <h2 className='section-title red'>Hiragana 3</h2>
+        <div className='row japan-row py-2'>
+          {renderWords('Hiragana 3')}
+        </div>
+        <hr/>
+        <h2 className='section-title purple'>Hiragana 4</h2>
+        <div className='row japan-row py-2'>
+          {renderWords('Hiragana 4')}
+        </div>
+        <hr/>
+        <h2 className='section-title yellow'>Greetings</h2>
+        <div className='row japan-row py-2'>
+          {renderWords('Greetings')}
         </div>
       </div>
     </div>
--- src/words.js
+++ src/words.js
@@ -106,5 +106,191 @@ export const words = {
       japanese: "こんにちは",
       english: "hello/good afternoon"
     }
+  ],
+  "Hiragana 3": [
+    {
+      japanese: [["犬", "いぬ"]],
+      english: "dog"
+    },
+    {
+      japanese: [["猫", "ねこ"]],
+      english: "cat"
+    },
+    {
+      japanese: [["鳥", "とり"]],
+      english: "bird"
+    },
+    {
+      japanese: [["耳", "みみ"]],
+      english: "ear"
+    },
+    {
+      japanese: [["背", "せ"],["中", "なか"]],
+      english: "back"
+    },
+    {
+      japanese: [["目", "め"]],
+      english: "eye"
+    },
+    {
+      japanese: [["夏", "なつ"]],
+      english: "summer"
+    },
+    {
+      japanese: [["冬", "ふゆ"]],
+      english: "winter"
+    },
+    {
+      japanese: [["晴", "は"], "れ"],
+      english: "sunny"
+    },
+    {
+      japanese: [["昼", "ひる"]],
+      english: "daytime"
+    },
+    {
+      japanese: [["曇", "くも"],"り"],
+      english: "cloudy"
+    },
+    {
+      japanese: [["家", "いえ"]],
+      english: "house"
+    },
+    {
+      japanese: [["部", "へ"],["屋", "や"]],
+      english: "room"
+    },
+    {
+      japanese: [["外", "そと"]],
+      english: "outside"
+    }
+  ],
+  "Hiragana 4": [
+    {
+      japanese: [["漫", "まん"],["画","が"]],
+      english: "manga"
+    },
+    {
+      japanese: [["銀", "ぎん"],["行","こう"]],
+      english: "bank"
+    },
+    {
+      japanese: [["絵", "え"],["文","も"],["字","じ"]],
+      english: "emoji"
+    },
+    {
+      japanese: [["地", "ち"],["図","ず"]],
+      english: "map"
+    },
+    {
+      japanese: [["風", "かぜ"]],
+      english: "wind"
+    },
+    {
+      japanese: [["果", "くだ"],["物", "もの"]],
+      english: "fruit"
+    },
+    {
+      japanese: [["電", "でん"],["話", "わ"]],
+      english: "phone"
+    },
+    {
+      japanese: [["鞄", "かばん"]],
+      english: "bag"
+    },
+    {
+      japanese: [["弁","べん"],["当","とう"]],
+      english: "bento/lunchbox"
+    },
+    {
+      japanese: [["鉛","えん"],["筆","ぴつ"]],
+      english: "pencil"
+    },
+    {
+      japanese: [["天", "てん"], "ぷら"],
+      english: "tempura"
+    },
+    {
+      japanese: [["九", "きゅう"]],
+      english: "nine"
+    },
+    {
+      japanese: [["今","きょ"],["日","う"]],
+      english: "today"
+    },
+    {
+      japanese: ["お",["茶","ちゃ"]],
+      english: "green tea"
+    },
+    {
+      japanese: [["切","きっ"],["符","ぷ"]],
+      english: "ticket"
+    },
+    {
+      japanese: [["学","がっ"],["校","こう"]],
+      english: "school"
+    },
+    {
+      japanese: [["納","なっ"],["豆","とう"]],
+      english: "fermented soybeans"
+    },
+    {
+      japanese: "ひらがな",
+      english: "hiragana"
+    },
+  ],
+  "Greetings": [
+    {
+      japanese: [["学","がく"],["生","せい"]],
+      english: "student"
+    },
+    {
+      japanese: [["先","せん"],["生","せい"]],
+      english: "teacher"
+    },
+    {
+      japanese: "どうも",
+      english: "thanks"
+    },
+    {
+      japanese: "ありがとう",
+      english: "thank you (informal)"
+    },
+    {
+      japanese: "ありがとうございます",
+      english: "thank you (formal)"
+    },
+    {
+      japanese: "どうもありがとう",
+      english: "thank you very much"
+    },
+    {
+      japanese: "おはようございます",
+      english: "good morning (formal)"
+    },
+    {
+      japanese: "ごめん",
+      english: "I'm sorry (informal)"
+    },
+    {
+      japanese: "ごめんなさい",
+      english: "I'm sorry (formal)"
+    },
+    {
+      japanese: ["お",["願","ねが"],"い"],
+      english: "please"
+    },
+    {
+      japanese: ["お",["願","ねが"],"いします"],
+      english: "please (formal)"
+    },
+    {
+      japanese: "よろしく",
+      english: "nice to meet you"
+    },
+    {
+      japanese: ["よろしくお",["願","ねが"],"いします"],
+      english: "nice to meet you (formal)"
+    },
   ]
 }
